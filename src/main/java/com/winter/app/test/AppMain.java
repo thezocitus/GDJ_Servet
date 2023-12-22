72e3114be79e2e6bf8647828734342c1e7fab6a9
Zominwoo <thezoticus@gmail.com>
2023-12-22 12:49:00 +0900

    DB

---
+++ src/main/java/com/winter/app/test/AppMain.java
@@ -0,0 +1,14 @@
+package com.winter.app.test;
+
+import com.winter.app.util.DBConnector;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+
+		DBConnector connector = new DBConnector();
+		connector.getConnetor();
+		
+	}
+
+}
